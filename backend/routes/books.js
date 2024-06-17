83479925354747d7013223b6dda948be8f394e6a
Niranjan A Pathani <141001142+niranjanpathani@users.noreply.github.com>
2024-06-17 13:50:30 +0530

    Delete backend directory

--- backend/routes/books.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getBooks, addBook } = require('../controllers/bookController');
-const auth = require('../middleware/auth');
-const router = express.Router();
-
-router.get('/', auth, getBooks);
-router.post('/', auth, addBook);
-
-module.exports = router;
